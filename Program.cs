cde306fe4417e01147f42d3804e4099ba1d7492e
stopbitch2442 <t.khmelevv@yandex.ru>
2023-01-27 00:19:17 +0300

    Lesson 4

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+var meetups = new List<Meetup>();
 
 if (app.Environment.IsDevelopment())
 {
@@ -12,8 +14,53 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.MapDelete("/meetups/{id:guid}", ([FromRoute] Guid id) =>
+{
+    var meetupToDelete = meetups.SingleOrDefault(meetup => meetup.Id == id);
+
+    if (meetupToDelete is null)
+    {
+        return Results.NotFound();
+    }
+
+    meetups.Remove(meetupToDelete);
+    return Results.Ok(meetupToDelete);
+});
+
+app.MapPost("/meetups", ([FromBody] Meetup newMeetup) =>
+{
+    newMeetup.Id = Guid.NewGuid();
+    meetups.Add(newMeetup);
+
+    return Results.Ok(newMeetup);
+});
+
+app.MapPut("/meetups/{id:guid}", ([FromRoute] Guid id, [FromBody] Meetup updatedMeetup) =>
+{
+    var oldMeetup = meetups.SingleOrDefault(meetup => meetup.Id == id);
+
+    if (oldMeetup is null)
+    {
+        return Results.NotFound();
+    }
+    oldMeetup.Topic = updatedMeetup.Topic;
+    oldMeetup.Place = updatedMeetup.Place;
+    oldMeetup.Duration = updatedMeetup.Duration;
+
+    return Results.NoContent();
+});
+
+app.MapGet("/meetups", () => Results.Ok(meetups));
 app.MapGet("/ping", () => Results.Ok("pong"));
 
 app.Run();
+
+class Meetup
+{
+    public Guid? Id { get; set; }
+    public string Topic { get; set; }
+    public string Place { get; set; }
+    public int Duration { get; set; }
+}
 
 
