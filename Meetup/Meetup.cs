d1e9fe15148749b72cba7af3815e87208bbd3d8b
stopbitch2442 <t.khmelevv@yandex.ru>
2023-01-27 23:42:06 +0300

    Lesson 6

---
+++ Meetup/Meetup.cs
@@ -0,0 +1,11 @@
+﻿namespace Meets.WebApi.Meetup
+{
+    internal class Meetup
+    {
+        
+            public Guid Id { get; set; }
+            public string Topic { get; set; }
+            public string Place { get; set; }
+            public int Duration { get; set; }
+    }
+}
